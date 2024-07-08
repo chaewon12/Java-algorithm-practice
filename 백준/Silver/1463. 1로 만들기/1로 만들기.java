ef0202887454b5c2bdb58fd49318d9f70d21e474
엄채원 <djacltk@naver.com>
2024-07-08 17:20:14 +0900

    [Silver III] Title: 1로 만들기, Time: 148 ms, Memory: 18036 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int N;
+	static int[] dp; // dp[i]는 정수 i를 1로 만들기 위한 최소 연산 횟수
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        if(N == 1){
+            System.out.println(0);
+            return;
+        }
+
+        dp = new int[N+1]; 
+
+        for(int i=2; i<=N; i++){
+            dp[i] = dp[i-1]+1;
+
+            // 2로 나누어 떨어지는 경우
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            // 3으로 나누어 떨어지는 경우
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        System.out.println(dp[N]);
+    }
+}
