c42204f6637237deec03b47645ef8f6398f84a6b
엄채원 <djacltk@naver.com>
2025-01-22 16:53:15 +0900

    [add] Filtering in JOIN or WHERE
    [고민] Filtering in JOIN or WHERE ?
    불필요한 데이터는 조인하지 않기 때문에 Filtering in JOIN 이 더 효율적이라고 생각한다
    하지만, 지금은 필터링 조건이 하나이기 때문애 가능하다고 생각이 된다.
    확장성을 고려한다면 WHERE절로 빼야할 것 같다
    
    1. Filtering in JOIN
    조인 단계에서 조건 평가 -> 불필요한 데이터 매칭 방지
    
    JOIN ECOLI_DATA parent
        ON parent.ID = curr.PARENT_ID
           AND (curr.GENOTYPE & parent.GENOTYPE) = parent.GENOTYPE
    
    2. Filtering in WHERE
    모든 데이터 조인 후 WHERE에서 조건 평가 -> 불필요한 데이터까지 조인, 이후 필터링 필요
    
    JOIN ECOLI_DATA parent
        ON parent.ID = curr.PARENT_ID
    WHERE (curr.GENOTYPE & parent.GENOTYPE) = parent.GENOTYPE

--- 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
+++ 프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -25,3 +25,23 @@ JOIN ECOLI_DATA parent
        AND (curr.GENOTYPE & parent.GENOTYPE) = parent.GENOTYPE
 ORDER BY curr.ID;
 
+/*
+[고민] Filtering in JOIN or WHERE ?
+불필요한 데이터는 조인하지 않기 때문에 Filtering in JOIN 이 더 효율적이라고 생각한다
+하지만, 지금은 필터링 조건이 하나이기 때문애 가능하다고 생각이 된다.
+확장성을 고려한다면 WHERE절로 빼야할 것 같다
+
+1. Filtering in JOIN
+조인 단계에서 조건 평가 -> 불필요한 데이터 매칭 방지
+
+JOIN ECOLI_DATA parent
+    ON parent.ID = curr.PARENT_ID
+       AND (curr.GENOTYPE & parent.GENOTYPE) = parent.GENOTYPE
+
+2. Filtering in WHERE
+모든 데이터 조인 후 WHERE에서 조건 평가 -> 불필요한 데이터까지 조인, 이후 필터링 필요
+
+JOIN ECOLI_DATA parent
+    ON parent.ID = curr.PARENT_ID
+WHERE (curr.GENOTYPE & parent.GENOTYPE) = parent.GENOTYPE
+*/
